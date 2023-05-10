fb22979d3fe8025a40737540c34a0b0c792581a5
Hector Cosgalla <valcos98@hotmail.com>
2023-05-10 00:31:03 -0600

    Creating a new proyect

---
+++ inventoryjavatry/src/main/java/com/valcos98/inventoryjavatry/InventoryjavatryApplication.java
@@ -0,0 +1,13 @@
+package com.valcos98.inventoryjavatry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryjavatryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryjavatryApplication.class, args);
+	}
+
+}
---
+++ inventoryjavatry/src/test/java/com/valcos98/inventoryjavatry/InventoryjavatryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valcos98.inventoryjavatry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryjavatryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
